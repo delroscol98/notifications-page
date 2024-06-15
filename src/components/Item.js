f36442da4e3fcd05f37000268a3d8b293f8d10db
delroscol98 <collindelrosario98@gmail.com>
2024-06-15 23:17:33 +1000

    JS and CSS files modified

---
+++ src/components/Item.js
@@ -0,0 +1,30 @@
+import "./Item.css";
+
+const Item = ({ item }) => {
+  return (
+    <div className={`item ${!item.read && "unread"}`}>
+      <img className="item__image" src={item.image} alt={item.name} />
+      <div className="item__details-box">
+        <p>
+          <span className="item__name ff-body-bold">{item.name}</span>
+          &nbsp;
+          <span className="item__details ff-body">{item.details}</span>
+          &nbsp;
+          <a href="#" className="item__link ff-body-bold">
+            {item.link}
+          </a>
+          {!item.read && <span className="item__unread">&nbsp;</span>}
+        </p>
+        <p className="item__time ff-body">{item.time}</p>
+        {item.message && (
+          <p className="item__message ff-body">{item.message}</p>
+        )}
+      </div>
+      {item.linkImg && (
+        <img className="item__linkImg" src={item.linkImg} alt="" />
+      )}
+    </div>
+  );
+};
+
+export default Item;
--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -7,93 +7,91 @@ import rizkyHasanuddinImg from "../assets/images/avatar-rizky-hasanuddin.webp";
 import kimberlySmithImg from "../assets/images/avatar-kimberly-smith.webp";
 import nathanPetersonImg from "../assets/images/avatar-nathan-peterson.webp";
 import annaKimImg from "../assets/images/avatar-anna-kim.webp";
+import chess from "../assets/images/image-chess.webp";
+
+import Item from "./Item";
 
 const notifications = [
   {
-    image: { markWebberImg },
+    image: markWebberImg,
     name: "Mark Webber",
     details: "reacted to your recent post",
     message: null,
     link: "My first tournament today!",
+    linkImg: null,
     time: "1m ago",
+    read: false,
   },
   {
-    image: { angelaGrayImg },
+    image: angelaGrayImg,
     name: "Angela Gray",
     details: "followed you",
     message: null,
     link: null,
+    linkImg: null,
     time: "5m ago",
+    read: false,
   },
   {
-    image: { jacobThompsonImg },
+    image: jacobThompsonImg,
     name: "Jacob Thompson",
     details: "has joined your group",
     message: null,
 
     link: "Chess club",
+    linkImg: null,
     time: "1 day ago",
+    read: false,
   },
   {
-    image: { rizkyHasanuddinImg },
+    image: rizkyHasanuddinImg,
     name: "Rizky Hasanuddin",
     details: "sent you a private message",
     message:
       "Hello, thanks for setting up the Chess Club. I’ve been a member for a few weeks now and I’m already having lots of fun and improving my game.",
     link: null,
+    linkImg: null,
     time: "5 days ago",
+    read: false,
   },
   {
-    image: { kimberlySmithImg },
+    image: kimberlySmithImg,
     name: "Kimberly Smith",
     details: "commented on your picture",
     message: null,
     link: null,
+    linkImg: chess,
     time: "1 week ago",
+    read: false,
   },
   {
-    image: { nathanPetersonImg },
+    image: nathanPetersonImg,
     name: "Nathan Peterson",
     details: "reacted to your recent post",
     message: null,
     link: "5 end-game strategies to increase your win rate",
+    linkImg: null,
     time: "2 weeks ago",
+    read: false,
   },
   {
-    image: { annaKimImg },
+    image: annaKimImg,
     name: "Anna Kim",
     details: "left the group",
     message: null,
     link: "Chess Club",
+    linkImg: null,
     time: "2 weeks ago",
+    read: false,
   },
 ];
 
 const Notifications = () => {
   return (
     <div className="notifications">
-      <div className="notification unread">
-        <img
-          className="notification__image"
-          src={markWebberImg}
-          alt="Mark Webber"
-        />
-        <div className="notification__details-box">
-          <p>
-            <span className="notification__name ff-body-bold">Mark Webber</span>
-            &nbsp;
-            <span className="notification__details ff-body">
-              reacted to your recent post
-            </span>
-            &nbsp;
-            <a href="#" className="notification__link ff-body-bold">
-              My first tournament today!
-            </a>
-            <span className="notification__unread">&nbsp;</span>
-          </p>
-          <p className="notification__time ff-body">1m ago</p>
-        </div>
-      </div>
+      {notifications.map((item) => {
+        return <Item item={item} key={item.name} />;
+      })}
     </div>
   );
 };
